64a3444ccd42aec0631cf672158de83932ebc863
Pavel <jys1670@hotmail.com>
2022-10-03 17:38:40 +0300

    Added formula/text cells

---
+++ src/cell.cpp
@@ -0,0 +1,86 @@
+#include "cell.h"
+
+#include <cassert>
+#include <string>
+#include <utility>
+
+Impl::Value EmptyImpl::GetValue() const
+{
+    return std::string{};
+}
+
+std::string EmptyImpl::GetText() const
+{
+    return {};
+}
+
+TextImpl::TextImpl(std::string text) : text_(std::move(text))
+{
+}
+
+Impl::Value TextImpl::GetValue() const
+{
+    if (!text_.empty() && text_.front() == '\'')
+    {
+        return text_.substr(1);
+    }
+    return text_;
+}
+
+std::string TextImpl::GetText() const
+{
+    return text_;
+}
+
+FormulaImpl::FormulaImpl(std::string text) : formula_(ParseFormula(std::move(text)))
+{
+}
+
+Impl::Value FormulaImpl::GetValue() const
+{
+    auto value = formula_->Evaluate();
+    if (std::holds_alternative<double>(value))
+    {
+        return Value(std::get<double>(value));
+    }
+
+    return Value(std::get<FormulaError>(value));
+}
+
+std::string FormulaImpl::GetText() const
+{
+    return "=" + formula_->GetExpression();
+}
+
+void Cell::Set(std::string text)
+{
+
+    if (text.empty())
+    {
+        impl_ = std::make_unique<EmptyImpl>();
+    }
+    else if (text.front() != FORMULA_SIGN || text.size() == 1)
+    { // '=' is not formula
+        impl_ = std::make_unique<TextImpl>(std::move(text));
+    }
+    else
+    {
+        auto tmp = std::make_unique<FormulaImpl>(text.substr(1));
+        impl_ = std::move(tmp);
+    }
+}
+
+void Cell::Clear()
+{
+    impl_ = std::make_unique<EmptyImpl>();
+}
+
+Cell::Value Cell::GetValue() const
+{
+    return impl_->GetValue();
+}
+
+std::string Cell::GetText() const
+{
+    return impl_->GetText();
+}
---
+++ src/cell.h
@@ -0,0 +1,70 @@
+
+#pragma once
+
+#include "common.h"
+#include "formula.h"
+
+#include <functional>
+#include <unordered_set>
+
+class Impl
+{
+  public:
+    using Value = std::variant<std::string, double, FormulaError>;
+
+    virtual ~Impl() = default;
+
+    virtual Value GetValue() const = 0;
+
+    virtual std::string GetText() const = 0;
+};
+
+class EmptyImpl : public Impl
+{
+  public:
+    Value GetValue() const override;
+
+    std::string GetText() const override;
+};
+
+class TextImpl : public Impl
+{
+  public:
+    explicit TextImpl(std::string text);
+
+    Value GetValue() const override;
+
+    std::string GetText() const override;
+
+  private:
+    std::string text_;
+};
+
+class FormulaImpl : public Impl
+{
+  public:
+    FormulaImpl(std::string text);
+
+    Value GetValue() const override;
+
+    std::string GetText() const override;
+
+  private:
+    std::unique_ptr<FormulaInterface> formula_;
+};
+
+class Cell : public CellInterface
+{
+  public:
+    Cell() = default;
+    ~Cell() override = default;
+
+    void Set(std::string text) override;
+    void Clear();
+
+    Value GetValue() const override;
+    std::string GetText() const override;
+
+  private:
+    std::unique_ptr<Impl> impl_{std::make_unique<EmptyImpl>()};
+};
